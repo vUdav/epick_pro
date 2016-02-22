2476d3a99bf69adfb9a881133e96a8c17633976a
vUdav <v.udav@bk.ru>
2016-02-22 21:12:14 +0300

    js relax block

--- build/js/script.js
+++ build/js/script.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
 	$('#menu-trigger').on('click',function(){
 		$('.main-nav__list').toggleClass('main-nav__list--active');
 	});
+
+	// relax block
+	$('.relax__tab a').on('click',function(e){
+		e.preventDefault();
+		var ident = $(this).attr('href');
+
+		$('.relax__tab').removeClass('relax__tab--active');
+		$(this).closest('.relax__tab').addClass('relax__tab--active');
+
+		$('.relax__slide').removeClass('relax__slide--active');
+		$(this).closest('.relax__slider').find(ident).addClass('relax__slide--active');
+	});
 });
